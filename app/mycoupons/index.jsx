52c8e80d435f4e840e92fabaae3fcb922a5a28ad
Backflipbro <rextabachnick@gmail.com>
2025-11-23 02:53:41 -0600

    added backend implementation

--- app/mycoupons/index.jsx
+++ app/mycoupons/index.jsx
@@ -1,13 +1,24 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
-import { styles } from "../stylesheet";
-import { useState } from "react";
 import Feather from "@expo/vector-icons/Feather";
 import { useRouter } from "expo-router";
-import { getCoupons, removeCoupon } from "../dummy_backend/couponsManager";
+import { useEffect, useState } from "react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { removeCoupon } from "../dummy_backend/couponsManager";
+import { styles } from "../stylesheet";
 
 const MyCouponsScreen = () => {
   const router = useRouter();
-  const [coupons, setCoupons] = useState(getCoupons());
+  const [coupons, setCoupons] = useState();
+
+  useEffect(() => {
+    fetch("http://192.168.1.xxx:8080/api/coupons")
+      .then(res => res.json())
+      .then(data => {
+        console.log("Fetched coupons:", data);
+        setCoupons(data);
+      })
+      .catch(err => console.error(err));
+  }, []);
+  
 
   return (
     <View style={styles.base}>
